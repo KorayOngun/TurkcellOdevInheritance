3212075d869ef352e6606046962ea357a9b05b38
koray öngün <koraygg28@gmail.com>
2023-03-19 21:50:46 +0300

    Proje dosyası ekle.

---
+++ AbstractClassInterface/Program.cs
@@ -0,0 +1,84 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // ürünleri veritabanına kaydeden program
+        Database[] databases = { new Sql(), new MySql("veritabanı") };
+        Product p1 = new() {Name="test"};
+        foreach (var item in databases)
+        {
+            item.Add(p1);
+            item.Update(p1);
+            item.Copy("a","b");
+            if (item is IPrint)
+            {
+                 IPrint _ = (IPrint)item;
+                 _.Print(p1);
+            }
+            Console.WriteLine("----------------------------------------------------------------------------------");
+        }
+    }
+}
+
+class Product
+{
+    public string Name { get; set; }    
+}
+
+
+abstract class Database
+{
+    private string _db;
+    public Database(string db)
+    {
+        _db = db; // <_db> copied <from> <to>
+    }
+    public abstract void Add(Product product);
+    public abstract void Update(Product product);   
+
+    public  void Copy(string from, string to)
+    {
+        Console.WriteLine($"{_db} copied from {from} to {to}");
+    }
+}
+interface IPrint
+{
+    void Print(Product product);
+}
+
+
+class Sql : Database, IPrint
+{
+    public Sql() : base("Sql") {}
+ 
+    public override void Add(Product product)
+    {
+        Console.WriteLine($"product {product.Name} Sql add");
+    }
+
+    public void Print(Product product)
+    {
+        Console.WriteLine($"product {product.Name} print");
+    }
+
+    public override void Update(Product product)
+    {
+        Console.WriteLine($"product {product.Name} Sql update");
+    }
+}
+
+
+
+class MySql : Database
+{
+    public MySql(string db = "MySql") : base(db) {}
+    public override void Add(Product product)
+    {
+        Console.WriteLine($"product {product.Name} MySql add");
+    }
+
+    public override void Update(Product product)
+    {
+        Console.WriteLine($"product {product.Name} MySql update");
+    }
+}
---
+++ ClassInterface/Program.cs
@@ -0,0 +1,76 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // işçilerin ne kadar doğum izni alacağını gösteren program
+        Employee[] employees = {new Employee(1,"ahmet"),new DadEmployee(2,"ali") ,new MomEmployee(3,"oya")};
+        foreach (var item in employees)
+        {
+            item.GetSalary();
+            item.HealthInsurance();
+            item.MaternityLeave();
+            if (item is IMaternety)
+            {
+                var _item=(IMaternety)item;
+                _item.MaternityInsurance();
+            }
+            Console.WriteLine("-------------------------------------------------------------------");
+        }
+
+    }
+}
+
+interface IMaternety
+{
+    void MaternityInsurance();
+}
+
+class  Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Employee(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public void GetSalary()
+    {
+        Console.WriteLine($"{Id} : {Name} paid salary ");
+    }
+    public void HealthInsurance()
+    {
+        Console.WriteLine($"{Id} : {Name} health insurance made ");
+    }
+    public virtual void MaternityLeave() 
+    {
+        Console.WriteLine($"no maternety leave");
+    }
+}
+
+class DadEmployee : Employee
+{
+    public DadEmployee(int id, string name) : base(id, name) { }
+
+    public override void MaternityLeave()
+    {
+        Console.WriteLine("10 days maternity leave");
+    }
+}
+
+class MomEmployee : Employee, IMaternety
+{
+    public MomEmployee(int id, string name) : base(id, name) { }
+
+    public void MaternityInsurance()
+    {
+        Console.WriteLine("Maternity insurance");
+    }
+
+    public override void MaternityLeave() 
+    {
+        Console.WriteLine("112 days maternity leave");    
+    }
+}
